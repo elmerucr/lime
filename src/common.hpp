f058fbbf85e66c1747f05b8129144350f8b65a0a
elmerucr <elmerucr@gmail.com>
2025-11-18 19:08:44 +0100

    build system

--- src/common.hpp
+++ src/common.hpp
@@ -13,7 +13,7 @@
 // ---------------------------------------------------------------------
 #define LIME_MAJOR_VERSION			0
 #define LIME_MINOR_VERSION			10
-#define LIME_BUILD					20251117
+#define LIME_BUILD					20251118
 #define LIME_YEAR					2025
 
 // ---------------------------------------------------------------------
